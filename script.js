387e50b470d3337c5e909b94e09b9a6594c03aed
Janet Webster <twixmixy@gmail.com>
2023-09-29 18:19:53 -0400

    Oops.. forgot to make a branch!

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-var words=["mouse"];
+var words= "mouse";
 var charactersGuessed=[];
 var startButton=document.querySelector(".start-button");
 var display=document.querySelector(".display");
-var displayWord=[];
+var displayWord;
 
-console.log("test");
-console.log(words[0].length);
+
 startButton.addEventListener("click",function(){
-    for (let i = 0; i < words[0].length; i++) {
-        displayWord+="_ ";
+    displayWord = [];
+    for (let i = 0; i < words.length; i++) {
+        displayWord+="_";
+        
     }
     display.textContent=displayWord;
+
 })
+
+document.addEventListener("keydown", function(e) {
+    charactersGuessed = e.key;
+    console.log(charactersGuessed);
+    var splitWord = words.split("");
+    displayWord = displayWord.split("");
+        console.log(displayWord);
+    for (let i = 0; i < words.length; i++) {
+        if (charactersGuessed === splitWord[i]) {
+            displayWord.splice(i, 1, charactersGuessed);
+            displayWord = displayWord.join("");
+            display.textContent = displayWord;
+        } else {
+
+        }
+    } 
+})
+
